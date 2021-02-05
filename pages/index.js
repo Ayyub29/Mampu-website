cb5200dac973efa2414b6cd1b3c6f1b89a4a3ebe
Muhammad Ayyub Abdurrahman <49425375+Ayyub29@users.noreply.github.com>
2021-02-05 12:09:06 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,66 @@
 import Head from 'next/head'
+
+import { fetchEntries } from '@util/contentfulPosts'
+
 import Header from '@components/Header'
 import Footer from '@components/Footer'
-import { fetchEntries } from '@util/contentfulPosts'
 import Post from '@components/Post'
+
+export default function Home({ posts }) {
+  return (
+    <div className="container">
+      <Head>
+        <title>Inisiatif Mampu</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <Header />
+        <div className="posts">
+          {posts.map((p) => {
+            return <Post key={p.date} date={p.date} image={p.image.fields} title={p.title} />
+          })}
+        </div>
+      </main>
+
+      <Footer />
+
+      <style jsx>{`
+        .container {
+          height: 100vh;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+        main {
+          padding: 5rem 0;
+          flex: 1;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+        .posts {
+          display: flex;
+        }
+      `}</style>
+
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu,
+            Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+        }
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
+    </div>
+  )
+}
 
 export async function getStaticProps() {
   const res = await fetchEntries()
@@ -16,28 +74,3 @@ export async function getStaticProps() {
     },
   }
 }
-
-export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Welcome to mampu</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <Header title="Welcome to Mampu!" />
-        <div className="posts">
-          {posts.map((p) => {
-            return <Post key={p.date} date={p.date} image={p.image.fields} title={p.title} />
-          })}
-        </div>
-      </main>
-
-      <Footer />
-    </div>
-  )
-}
-
-
-

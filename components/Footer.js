f3c3b5150e0c2bbbd42c1a497f0d2cecf235fafc
gilliantuerah <gilliantuerah@gmail.com>
2021-02-13 22:32:43 +0700

    responsiveness again bro

--- components/Footer.js
+++ components/Footer.js
@@ -8,30 +8,23 @@ export default function Footer() {
         <img src="/mampu-icon.svg" alt="Mampu Logo" className={styles.mampuLogo} />
           <div class={styles.headquarterText}>
               <p class={styles.title}>Headquarters</p>
-              <p class={styles.desc}>Jl. Alamat 1 No.23, Jakarta Selatan, DKI Jakarta </p>
-          </div>
-          <div class={styles.contactText}>
-              <p class={styles.title}>Contact</p>
-              <p class={styles.desc}> 
-                  <img src="/whatsapp-icon.svg" alt="Whatsapp icon" className={styles.whatsappLogo} /> &#160;
-                  +62-8515-515-8299 (Albert)
-              </p>
+              <p class={styles.desc}>Jl. Klampis Anom no. 29 Sukolilo, Surabaya, 60117</p>
           </div>
           <div class={styles.followusText}>
               <p class={styles.title}>Follow Us</p>
               <a href="mailto:contact@mampu.org" class={styles.desc}> 
                   <img src="/pesan-icon.svg" alt="Email icon" className={styles.whatsappLogo} /> &#160;
-                  contact@mampu.org 
+                  <p>contact@mampu.org</p> 
               </a>
               <br></br>
               <a href="https://www.instagram.com/inisiatif.mampu/"  class={styles.desc}> 
                   <img src="/instagram-icon.svg" alt="Instagram icon" className={styles.whatsappLogo} /> &#160;
-                  @inisiatif.mampu /@mampu.initiative
+                  <p>@inisiatif.mampu /@mampu.initiative</p>
               </a>
               <br></br>
               <a href="https://www.linkedin.com/company/mampu-initiative/"  class={styles.desc}> 
                   <img src="/linkedin-icon.svg" alt="Linkedin icon" className={styles.whatsappLogo} /> &#160;
-                  MAMPU Initiative
+                  <p>MAMPU Initiative</p>
               </a>
           </div>
         </div>

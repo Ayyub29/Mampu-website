583475c4bcf05a1138cad9b0ca4d6fc31606b008
gilliantuerah <gilliantuerah@gmail.com>
2021-03-01 18:58:24 +0700

    famili on mobile view

--- components/Families/FamiliesMember.js
+++ components/Families/FamiliesMember.js
@@ -23,59 +23,59 @@ export default function FamiliesMember({currentFamily, pastFamily}) {
                 </div>
             </div>
             
-                {/* <ul className={styles.ul}> */}
-                    <div className={styles.li} style={{
-                        backgroundImage: show1? "url(/curr-fam-bg.png)":"none", 
-                        backgroundPositionY: "bottom",
-                        backgroundSize: "cover",
-                        backgroundRepeat: "no-repeat",
-                        marginTop: show1? "0px":"20px"
-                    }}>
+            <div className={styles.famList}>
+                <div className={styles.li} style={{
+                    backgroundImage: show1? "url(/curr-fam-bg.png)":"none", 
+                    backgroundPositionY: "bottom",
+                    backgroundSize: "cover",
+                    backgroundRepeat: "no-repeat",
+                    marginTop: show1? "0px":"20px"
+                }}>
 
-                        <button 
-                            onClick={toggle1} 
-                            className={styles.famItem} 
-                            style={{color: show1? "#A6C8E8":"#383769"}} > 
-                            <img src={show1? "/down-arrow.png":"/show-toggler.png"} className={styles.famToggler}/> 
-                            Current Families
-                        </button>
-                        <div style={{display: show1? "flex":"none"}} className={styles.famSection}>
-                            <CardColumns className={styles.cardColumn}>
-                                {currentFamily.map((a) =>(
-                                    <Card className={styles.cardFamContainer}>
-                                        <Card.Img variant="top" src={`https:${a.familyPhoto.fields.file.url}`} className={styles.imgFam}/>
-                                        <Card.Body className={styles.descFam}>
-                                            <Card.Title className={styles.titleFam}>{a.familyName}</Card.Title>
-                                            <Card.Text className={styles.textFam}>
-                                                {a.familyDesc}
-                                            </Card.Text>
-                                        </Card.Body>
-                                    </Card>
-                                ))}
+                    <button 
+                        onClick={toggle1} 
+                        className={styles.famItem} 
+                        style={{color: show1? "#A6C8E8":"#383769"}} > 
+                        <img src={show1? "/down-arrow.png":"/show-toggler.png"} className={styles.famToggler}/> 
+                        Current Families
+                    </button>
+                    <div style={{display: show1? "flex":"none"}} className={styles.famSection}>
+                        <CardColumns className={styles.cardColumn}>
+                            {currentFamily.map((a) =>(
+                                <Card className={styles.cardFamContainer}>
+                                    <Card.Img variant="top" src={`https:${a.familyPhoto.fields.file.url}`} className={styles.imgFam}/>
+                                    <Card.Body className={styles.descFam}>
+                                        <Card.Title className={styles.titleFam}>{a.familyName}</Card.Title>
+                                        <Card.Text className={styles.textFam}>
+                                            {a.familyDesc}
+                                        </Card.Text>
+                                    </Card.Body>
+                                </Card>
+                            ))}
 
-                            </CardColumns>
-                        </div>
+                        </CardColumns>
                     </div>
-                    <div className={styles.li} style={{marginTop: show1? "50px":"20px"}}>
-                        <button onClick={toggle2} className={styles.famItem}> <img src="/show-toggler.png" style={{transform: show2? "rotate(90deg)":"none"}} className={styles.famToggler}/> Past Families</button>
-                        <div style={{display: show2? "flex":"none"}} className={styles.famSection}>
-                            <CardColumns className={styles.cardColumn}>
-                                {pastFamily.map((a) =>(
-                                    <Card className={styles.cardFamContainer}>
-                                        <Card.Img variant="top" src={`https:${a.familyPhoto.fields.file.url}`} className={styles.imgFam}/>
-                                        <Card.Body className={styles.descFam}>
-                                            <Card.Title className={styles.titleFam}>{a.familyName}</Card.Title>
-                                            <Card.Text className={styles.textFam}>
-                                                {a.familyDesc}
-                                            </Card.Text>
-                                        </Card.Body>
-                                    </Card>
-                                ))}
+                </div>
+                <div className={styles.li} style={{marginTop: show1? "50px":"20px"}}>
+                    <button onClick={toggle2} className={styles.famItem}> <img src="/show-toggler.png" style={{transform: show2? "rotate(90deg)":"none"}} className={styles.famToggler}/> Past Families</button>
+                    <div style={{display: show2? "flex":"none"}} className={styles.famSection}>
+                        <CardColumns className={styles.cardColumn}>
+                            {pastFamily.map((a) =>(
+                                <Card className={styles.cardFamContainer}>
+                                    <Card.Img variant="top" src={`https:${a.familyPhoto.fields.file.url}`} className={styles.imgFam}/>
+                                    <Card.Body className={styles.descFam}>
+                                        <Card.Title className={styles.titleFam}>{a.familyName}</Card.Title>
+                                        <Card.Text className={styles.textFam}>
+                                            {a.familyDesc}
+                                        </Card.Text>
+                                    </Card.Body>
+                                </Card>
+                            ))}
 
-                            </CardColumns>
-                        </div>
+                        </CardColumns>
                     </div>
-                {/* </ul> */}
+                </div>
+            </div>
             <div className={styles.ombakKanan} style={{marginTop: show2? "50px":"300px"}}>
                 <img src="/footer-fam.png" alt="vector" className={styles.ombakImg}/>
             </div>
